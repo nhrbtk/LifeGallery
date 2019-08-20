0a2f11474a9ae2047853397849a16fc62eeb7310
nhrbtk <39525068+nhrbtk@users.noreply.github.com>
2019-08-20 19:49:39 +0300

    Removed

--- Backend/LG.WEB/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace LG.WEB.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Backend/LG.WEB/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LG.WEB.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
